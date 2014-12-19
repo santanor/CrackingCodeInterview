ceb36acf158b63db3922d2c1959d12fe19f4d4da
Menti <josementi@gmail.com>
2014-12-19 11:18:34 +0100

    String Compression

---
+++ CrackingTheCodeInterview/StringCompression/StringCompression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompression
+{
+    public class StringCompressor
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string Compress(string text)
+        {
+            if (text == null) return "";
+            if (text == "" || text.Length == 1 || text.Length == 2)
+                return text;
+            char[] chars = text.ToCharArray();
+            int currentChar = 0;
+            StringBuilder sb = new StringBuilder();
+            while(currentChar < text.Length)
+            {
+                int consecutiveChars = consecutive(chars, chars[currentChar], currentChar);
+                sb.Append(consecutiveChars);
+                sb.Append(chars[currentChar]);
+                currentChar += consecutiveChars;
+            }
+
+            if (text.Length <= sb.ToString().Length)
+                return text;
+            else return sb.ToString();
+        }
+
+        private int consecutive(char[] chars, char charac, int current)
+        {
+            int counter = 0;
+            int index = current;
+            while (index < chars.Length)
+            {
+                if (chars[index] == charac)
+                {
+                    index++;
+                    counter++;
+                }
+                else
+                    return counter;
+            }
+            
+            return counter;
+        }
+    }
+}
---
+++ CrackingTheCodeInterview/UnitTestInterviewCode/StringCompressionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCompression;
+
+namespace UnitTestInterviewCode
+{
+    [TestClass]
+    public class StringCompressionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StringCompressor sc = new StringCompressor();
+
+            Assert.AreEqual<string>("2a1b5c3b", sc.Compress("aabcccccbbb"));
+            Assert.AreEqual<string>("aa", sc.Compress("aa"));
+            Assert.AreEqual<string>("", sc.Compress(""));
+            Assert.AreEqual<string>("", sc.Compress(null));
+            Assert.AreEqual<string>("abccdefg", sc.Compress("abccdefg"));
+            Assert.AreEqual<string>("3a3b1g1c3t3s2f", sc.Compress("aaabbbgctttsssff"));
+        }
+    }
+}
